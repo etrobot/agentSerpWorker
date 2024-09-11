7e2e70585fe4de613a603f7432087101a12bb12b
etrobot@outlook.com <etrobot@outlook.com>
2024-09-11 17:38:46 +0800

    improve prompt

--- agents/planner.py
+++ agents/planner.py
@@ -3,9 +3,9 @@ from langchain_openai import ChatOpenAI
 
 #prompt
 plannerPrompt:str = """
-For the given object, make a group of keywords for the search engine. \
+For the given object, Take a deep breath and think step by step, what's the related concept is needed to finish the search? make a group of keywords for the search engine. \
 for example, the object is "write a wiki for google.com",and the keywors group should be in array format: \
-["google.com funciton","google.com history","google.com tech stack","google.com bussiness mode"]
+["search engine","search engine bussiness mode","alphabet investment value","google.com tech stack","google.com founder"]
 
 Here's the object: 
 
